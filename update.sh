09b70c1a0bd46e77933a72478c417636d03d0d87
Colin Cheng <zbinlin@gmail.com>
2021-02-25 22:40:22 +0800

    FEAT: compatible with dash

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 set -o allexport
-[[ -f .env ]] && . .env
+[ -f .env ] && . ./.env
 set +o allexport
 
 ENDPOINT=https://api.cloudflare.com/client/v4/
@@ -14,7 +14,7 @@ DOMAIN=${1:-$DOMAIN}
 IP=$(\
 	ip -6 address show scope global \
 	| awk '{ print $2 }' \
-  	| grep \
+	| grep \
 		-e '^200[1,3]' \
 		-e '^2[4,6,8,c]' \
 		-e '^26[1-3]' \
@@ -23,56 +23,59 @@ IP=$(\
 )
 IP=${IP%/*}
 
-function log() {
-	if [[ -n "${DEBUG++}" ]];
+if [ -z "$IP" ];
+then
+	echo "IPv6 address not found" >&2
+	exit 1
+fi
+
+
+log() {
+	if [ -n "${DEBUG++}" ];
 	then
 		echo "$@" >&2
 	fi
 }
 
-function fetch() {
-	if [[ "$1" == "" ]];
+fetch() {
+	method="GET"
+	path=${1:?Missing argument 1}
+	if [ -n "$2" ];
 	then
-		echo "Missing argument 1" >&2
-		return 1
-	fi
-	if [[ "$2" == "" ]];
-	then
-		method="GET"
-	else
 		method="POST"
 		payload=$2
 	fi
-	if [[ "$3" != "" ]];
+	if [ -n "$3" ];
 	then
-		method=$payload
+		method=$1
+		path=$2
 		payload=$3
 	fi
 
-	log $method $1 "$payload"
+	log "method=$method" "path=$path" "payload=$payload"
 
 	res=$( \
-		curl -sSL -X "$method" "${ENDPOINT}$1" \
+		curl -sSL -X "$method" "${ENDPOINT}$path" \
 			-H "Authorization: Bearer ${API_TOKEN}" \
 			-H "Content-Type:application/json" \
 			--data-raw "$payload" \
 	)
 
-	if [[ "true" == "$(jq -r '.success' <<<$res)" ]];
+	if [ "true" = "$(echo -n $res | jq -r '.success')" ];
 	then
 		echo "$res" | jq -r '.result'
 	else
 		echo "$res" >&2
-		exit 1
+		return 1
 	fi
 }
 
-function list() {
+list() {
 	name=${1:+&name=$1}
 	fetch "zones/${ZONE_ID}/dns_records?type=AAAA${name}"
 }
 
-function create() {
+create() {
 	: ${1:?Argument 1 must be a domain name}
 	: ${2:?Argument 2 must be a IPv6 address}
 	log "Create DNS Record(name: $1) to $2"
@@ -88,11 +91,11 @@ function create() {
 	log "$result"
 }
 
-function update() {
+update() {
 	: ${1:?Argument 1 must be a dns_records id}
 	: ${2:?Argument 2 must be a IPv6 address}
 	log "Update DNS Record(id: $1) to $2"
-	result=$(fetch "zones/${ZONE_ID}/dns_records/$1" "PATCH" '
+	result=$(fetch "PATCH" "zones/${ZONE_ID}/dns_records/$1" '
 {
 	"content": "'$2'"
 }
@@ -100,16 +103,19 @@ function update() {
 	log "$result"
 }
 
-function upsert() {
+upsert() {
 	: ${1:?Argument 1 must be a domain name}
 	: ${2:?Argument 2 must be a IPv6 address}
-	rid=$(list $1 | jq -r '.[0].id')
-	if [[ "null" == "$rid" ]];
+
+	lst=$(list "$1")
+	rid=$(echo -n "$lst" | jq -r '.[0].id')
+
+	if [ "null" = "$rid" ];
 	then
-		create $1 $2
+		create "$1" "$2"
 	else
-		update $rid $2
+		update "$rid" "$2"
 	fi
 }
 
-upsert $DOMAIN $IP
+upsert "$DOMAIN" "$IP"
